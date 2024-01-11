ff9214df9f6102c3149a204f04fa823f41792625
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-11 17:34:50 -0500

    Delete src directory

--- src/actions/actions.js
+++
@@ -1,6 +0,0 @@
-import data from '../data.json'
-
-export const fetchData = () => ({
-  type: 'FETCH_RETAIL_SALES',
-  payload: data
-})
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers/rootReducer'
-import { fetchData } from '../actions/actions'
-
-const store = createStore(rootReducer)
-
-store.dispatch(fetchData())
-
-export default store

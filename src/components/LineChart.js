ef850885c3913ec7c1e1ba2f6c01de8194c27bb2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-12 06:41:45 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -3,9 +3,13 @@ import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts'
 import { formatChartDate } from '../utilities/formatDate'
 import Dropdown from './Dropdown'
 
+const months = [
+  'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+]
+
 const LineChartComponent = ({ data }) => {
   const [state, setState] = useState([])
-  const [uniqueMonths, setUniqueMonths] = useState([])
+  //const [uniqueMonths, setUniqueMonths] = useState([])
   const [adjustedMinValue, setAdjustedMinValue] = useState([])
   const [adjustedMaxValue, setAdjustedMaxValue] = useState([])
   const [showSales, setShowSales] = useState({
@@ -31,7 +35,7 @@ const LineChartComponent = ({ data }) => {
     const type = getType(event.currentTarget.lastChild.textContent)
     setShowSales(prevState => ({ ...prevState, [type]: !prevState[type] }))
   }
-  
+ /* 
   const formatData = array => {
     return array.map(item => ({ 
       weekEnding: formatChartDate(item.weekEnding),
@@ -41,6 +45,7 @@ const LineChartComponent = ({ data }) => {
       retailerMargin: item.retailerMargin
     }))
   }
+  */
 
   const adjustYAxisHeight = () => {
     const min = Math.min(...data.map(item => item.retailSales))
@@ -53,10 +58,10 @@ const LineChartComponent = ({ data }) => {
   }
 
   const initializeState = () => {
-    const init = formatData(data)
-    const months = [...new Set(init.map(item => item.weekEnding))]
-    setState(init)
-    setUniqueMonths(months)
+    //const init = formatData(data)
+    //const months = [...new Set(init.map(item => item.weekEnding))]
+    //setState(init)
+    //setUniqueMonths(months)
     adjustYAxisHeight()
   }
 
@@ -69,14 +74,14 @@ const LineChartComponent = ({ data }) => {
       <h3>Retail Sales</h3>
       <Dropdown onClick={handleDropdown} selected={showSales} />
       <ResponsiveContainer width="100%" height={530}>
-        <LineChart data={state}>
+        <LineChart data={data}>
           <Line type="monotone" dataKey="retailSales" stroke="#44A8F6" strokeWidth={3} dot={false} x1="10%" x2="50%" />
           <YAxis hide={true} domain={[adjustedMinValue, adjustedMaxValue]} />
           {showSales.wholesale && <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={3} dot={false} />}
           {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={3} dot={false} />}
           {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={3} dot={false} />}
-          <line x1="0%" y1="90%" x2="100%" y2="90%" stroke="#eeeeee" strokeWidth="1" />
-          <XAxis 
+         {/* <line x1="0%" y1="90%" x2="100%" y2="90%" stroke="#eeeeee" strokeWidth="1" />
+             <XAxis 
             dataKey="weekEnding" 
             axisLine={false} 
             tickLine={false} 
@@ -84,11 +89,13 @@ const LineChartComponent = ({ data }) => {
             tick={{ fill: '#ABB8C8', fontSize: '14px' }}
             dx={-5} 
             padding={{ left: 35, right: 35 }} 
-          />
+          /> */}
         </LineChart>
       </ResponsiveContainer>
-      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0 35px' }}>
-        {uniqueMonths.map(item => <span>{item}</span>)}
+      <div style={{ borderTop: '1px solid #eee' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0 35px' }}>
+          {months.map(item => <span>{item}</span>)}
+        </div>
       </div>
     </div>
   )

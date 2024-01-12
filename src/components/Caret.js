63ad0a99ad39ea59bd79b0f7c61e62ab0dc9b95e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-12 09:06:20 -0500

    Update Caret.js

--- src/components/Caret.js
+++ src/components/Caret.js
@@ -1,7 +1,7 @@
 import CaretDownIcon from '../icons/CaretDownIcon'
 import CaretUpIcon from '../icons/CaretUpIcon'
 
-const Caret = ({ direction }) => {
+const Caret = ({ direction = 'down' }) => {
   return direction === 'down' ? <CaretDownIcon /> : <CaretUpIcon />
 }
 

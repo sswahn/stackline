e70c4fc18dcf52c4ce46f1a413985041edae913c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-12 11:32:31 -0500

    Update Caret.js

--- src/components/Caret.js
+++ src/components/Caret.js
@@ -2,7 +2,7 @@ import CaretDownIcon from '../icons/CaretDownIcon'
 import CaretUpIcon from '../icons/CaretUpIcon'
 
 const Caret = ({ id, key, direction }) => {
-  return key === id && (isDescending ? <CaretDownIcon /> : <CaretUpIcon />) || <CaretDownIcon />
+  return key === id && (direction ? <CaretDownIcon /> : <CaretUpIcon />) || <CaretDownIcon />
 }
 
 export default Caret

7e45c804365358cc49677e521db5760d9eb68f61
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-12 08:21:22 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -7,8 +7,8 @@ const months = [
 ]
 
 const LineChartComponent = ({ data }) => {
-  const [adjustedMinValue, setAdjustedMinValue] = useState([])
-  const [adjustedMaxValue, setAdjustedMaxValue] = useState([])
+  const [adjustedBase, setAdjustedBase] = useState([])
+  const [adjustedPeak, setAdjustedPeak] = useState([])
   const [showSales, setShowSales] = useState({
     wholesale: false,
     unitsSold: false,
@@ -21,13 +21,12 @@ const LineChartComponent = ({ data }) => {
   }
 
   const adjustYAxisHeight = () => {
-    const min = Math.min(...data.map(item => item.retailSales))
-    const max = Math.max(...data.map(item => item.retailSales))
+    const retailSales = data.map(item => item.retailSales)
+    const min = Math.min(...retailSales)
+    const max = Math.max(...retailSales)
     const range = 0.2 // Adjust range to control line height
-    const minValue = min - (max - min) * (1 - range)
-    const maxValue = max + (max - min) * (1 - range)
-    setAdjustedMinValue(minValue)
-    setAdjustedMaxValue(maxValue)
+    setAdjustedBase(min - (max - min) * (1 - range))
+    setAdjustedPeak(max + (max - min) * (1 - range))
   }
 
   useEffect(() => {
@@ -44,7 +43,7 @@ const LineChartComponent = ({ data }) => {
           {showSales.wholesale && <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} />}
           {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} />}
           {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} />}
-          <YAxis hide={true} domain={[adjustedMinValue, adjustedMaxValue]} />
+          <YAxis hide={true} domain={[adjustedBase, adjustedPeak]} />
           <XAxis hide={true} padding={{ left: 35, right: 35 }} />
         </LineChart>
       </ResponsiveContainer>

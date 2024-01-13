8682d4ba0c811ff6277c7a0ad9b688ef5ca4f8b0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-13 18:14:03 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,4 +3,9 @@ export const config = {
   months: [
     'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
   ],
+  sales: {
+    wholesale: 'Wholesale Sales',
+    units: 'Units Sold',
+    margin: 'Retailer Margin'
+  }
 }

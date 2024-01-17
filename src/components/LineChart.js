ddc2f5bccd74d3b4a7e4647cff3cba58931ebe3e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 09:56:50 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts'
 import { config } from '../config'
 import Dropdown from './Dropdown'
 import CustomTooltip from './CustomTooltip'
 
 const LineChartComponent = ({ data }) => {
+  const [adjustedBase, setAdjustedBase] = useState([])
+  const [adjustedPeak, setAdjustedPeak] = useState([])
   const [showSales, setShowSales] = useState({
     wholesale: false,
     unitsSold: false,
@@ -15,6 +17,19 @@ const LineChartComponent = ({ data }) => {
     const id = event.currentTarget.dataset.id
     setShowSales(prevState => ({ ...prevState, [id]: !prevState[id] }))
   }
+
+  const adjustYAxisHeight = () => {
+    const retailSales = data.map(item => item.retailSales)
+    const min = Math.min(...retailSales)
+    const max = Math.max(...retailSales)
+    const range = 0.2 // Adjust range to control line height
+    setAdjustedBase(min - (max - min) * (1 - range))
+    setAdjustedPeak(max + (max - min) * (1 - range))
+  }
+
+  useEffect(() => {
+    adjustYAxisHeight()
+  }, [data])
   
   return (
     <section className="retail-chart panel">
@@ -26,7 +41,7 @@ const LineChartComponent = ({ data }) => {
           <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} stackId="sales" isAnimationActive={false} />
           {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} stackId="sales" isAnimationActive={false} />}
           {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} stackId="sales" isAnimationActive={false} />}
-          <YAxis hide={true} />
+          <YAxis hide={true} domain={[adjustedBase, adjustedPeak]} />
           <XAxis hide={true} dataKey="weekEnding" padding={{ left: 35, right: 35 }} />
           {/* <Tooltip content={<CustomTooltip />} /> */}
         </LineChart>

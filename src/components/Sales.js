02439f7444caa9248f2b526d9f8bacde1d6942b2
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 16:17:05 -0500

    Update Sales.js

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux'
 import Sidebar from './Sidebar'
-import Chart from './Chart'
+import LineChart from './LineChart'
 import Table from './Table'
 
 const Sales = () => {
@@ -30,7 +30,7 @@ const Sales = () => {
     <div className="retail-sales">
       <Sidebar data={data} />
       <div>
-        <Chart data={normalizedData} /> 
+        <LineChart data={normalizedData} /> 
         <Table data={sales} />
       </div>
     </div>

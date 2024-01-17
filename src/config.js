7d476c3c7f3b137b36a46ea56352c3892daa2f46
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 17:11:30 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,48 +6,5 @@ export const config = {
     wholesaleSales: 'Wholesale Sales',
     unitsSold: 'Units Sold',
     retailerMargin: 'Retailer Margin'
-  },
-  options: {
-    scales: {
-      x: {
-        display: false,
-        grid: {
-          display: false
-        },
-      },
-      y: {
-        stacked: true,
-        display: false,
-        grid: {
-          display: false
-        }
-      }
-    },
-    elements: {
-      line: {
-        cubicInterpolationMode: 'monotone'
-      },
-      point: {
-        radius: 0
-      }
-    },
-    plugins: {
-      legend: {
-        display: false
-      },
-      tooltip: {
-        enabled: true
-      }
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false
-    },
-    layout: {
-      padding: {
-        left: 35,
-        right: 35
-      }
-    }
   }
 }

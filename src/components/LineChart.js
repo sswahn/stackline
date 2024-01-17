6e6d69e74259fadbe31782601f17854dd2340f31
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 07:54:04 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -26,7 +26,7 @@ const LineChartComponent = ({ data }) => {
           {showSales.wholesale && <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} stackId="sales" />}
           {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} stackId="sales" />}
           {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} stackId="sales" />}
-          <YAxis hide={true} domain={[0, 475 / 2]} />
+          <YAxis hide={true} interval="preserveStartEnd" tickCount={475 / 2} />
           <XAxis hide={true} dataKey="weekEnding" padding={{ left: 35, right: 35 }} />
           <Tooltip content={<CustomTooltip />} />
         </LineChart>

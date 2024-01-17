15fc209b5c27fb6663f7ecd37cca038420daa30c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 16:39:24 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -39,10 +39,10 @@ const LineChartComponent = ({ data }) => {
       <Dropdown onClick={handleDropdown} selected={showSales} />
       <ResponsiveContainer height={475}>
         <LineChart data={data}>
-          <Line type="monotone" dataKey="retailSales" stroke="#44A8F6" strokeWidth={4} dot={false} stackId="1" isAnimationActive={false} />
-          {showSales.wholesaleSales && <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} stackId="1" isAnimationActive={false} />}
-          {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} stackId="1" isAnimationActive={false} />}
-          {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} stackId="1" isAnimationActive={false} />}
+          <Line type="monotone" dataKey="retailSales" stroke="#44A8F6" strokeWidth={4} dot={false} stackId="1" stackOffset="expand" isAnimationActive={false} />
+          {showSales.wholesaleSales && <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} stackId="1" stackOffset="expand" isAnimationActive={false}  />}
+          {showSales.unitsSold && <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} stackId="1" stackOffset="expand" isAnimationActive={false} />}
+          {showSales.retailerMargin && <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} stackId="1" stackOffset="expand" isAnimationActive={false} />}
           <YAxis hide={true} type="number" />
           <XAxis hide={true} dataKey="weekEnding" padding={{ left: 35, right: 35 }} />
           {/*<Tooltip content={<CustomTooltip />} /> */}

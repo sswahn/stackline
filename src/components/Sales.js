a33faba9e404e3474d187e606316dacc36943434
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 11:31:32 -0500

    Update Sales.js

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -5,27 +5,26 @@ import Table from './Table'
 
 const Sales = () => {
   const { sales, ...data } = useSelector(state => state.data.retailSales).at(0)
+  
   const totalMonetaryValue = sales.reduce((acc, val) => acc + val.retailSales + val.wholesaleSales + val.retailerMargin, 0)
+  
   const totalUnitsSold = sales.reduce((acc, val) => acc + val.unitsSold, 0)
+  
   const percentages = sales.map(item => ({
     retailSales: item.retailSales / totalMonetaryValue * 100,
     wholesaleSales: item.wholesaleSales / totalMonetaryValue * 100,
     unitsSold: item.unitsSold / totalUnitsSold * 100,
     retailerMargin: item.retailerMargin / totalMonetaryValue * 100
   }))
+  
   const maxPercentage = Math.max(...percentages.map(item => Object.values(rest)).flat())
-
-  console.log('maxPercentage: ', maxPercentage)
-
+  
   const normalizedData = percentages.map((item, index) => ({
-    weekEnding: sales[index].weekEnding,
     retailSales: item.retailSales / maxPercentage,
     wholesaleSales: item.wholesaleSales / maxPercentage,
     unitsSold: item.unitsSold / maxPercentage,
     retailerMargin: item.retailerMargin / maxPercentage
   }))
-
-  console.log('normalizedData: ', normalizedData)
 
   return (
     <div className="retail-sales">

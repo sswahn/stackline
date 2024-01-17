59bdc038305440233c4959979dafe17510b2a09a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 07:28:11 -0500

    Update LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -5,8 +5,11 @@ import Dropdown from './Dropdown'
 import CustomTooltip from './CustomTooltip'
 
 const LineChartComponent = ({ data }) => {
-  const [adjustedBase, setAdjustedBase] = useState([])
-  const [adjustedPeak, setAdjustedPeak] = useState([])
+  const [retailYAxis, setRetailYAxis] = useState({})
+  const [wholesaleYAxis, setWholesaleYAxis] = useState({})
+  const [unitsSoldYAxis, adjustUnitsSoldYAxis] = useState({})
+  const [retailerMarginYAxis, adjustRetailerMarginYAxis] = useState({})
+  
   const [showSales, setShowSales] = useState({
     wholesale: false,
     unitsSold: false,
@@ -18,17 +21,55 @@ const LineChartComponent = ({ data }) => {
     setShowSales(prevState => ({ ...prevState, [id]: !prevState[id] }))
   }
 
-  const adjustYAxisHeight = () => {
+  const adjustRetailYAxis = () => {
     const retailSales = data.map(item => item.retailSales)
     const min = Math.min(...retailSales)
     const max = Math.max(...retailSales)
     const range = 0.1 // Adjust range to control line height
-    setAdjustedBase(min - (max - min) * (1 - range))
-    setAdjustedPeak(max + (max - min) * (1 - range))
+    setRetailYAxis({
+      base: min - (max - min) * (1 - range),
+      height: max + (max - min) * (1 - range)
+    })
+  }
+
+  const adjustWholesaleYAxis = () => {
+    const retailSales = data.map(item => item.wholesaleSales)
+    const min = Math.min(...retailSales)
+    const max = Math.max(...retailSales)
+    const range = 0.1 // Adjust range to control line height
+    setWholesaleYAxis({
+      base: min - (max - min) * (1 - range),
+      height: max + (max - min) * (1 - range)
+    })
+  }
+
+  const adjustUnitsSoldYAxis = () => {
+    const retailSales = data.map(item => item.unitsSold)
+    const min = Math.min(...retailSales)
+    const max = Math.max(...retailSales)
+    const range = 0.1 // Adjust range to control line height
+    setUnitsSoldYAxis({
+      base: min - (max - min) * (1 - range),
+      height: max + (max - min) * (1 - range)
+    })
+  }
+
+  const adjustRetailerMarginYAxis = () => {
+    const retailSales = data.map(item => item.retailerMargin)
+    const min = Math.min(...retailSales)
+    const max = Math.max(...retailSales)
+    const range = 0.1 // Adjust range to control line height
+    setRetailerMarginYAxis({
+      base: min - (max - min) * (1 - range),
+      height: max + (max - min) * (1 - range)
+    })
   }
 
   useEffect(() => {
     adjustYAxisHeight()
+    adjustWholesaleYAxis()
+    adjustUnitsSoldYAxis()
+    adjustRetailerMarginYAxis()
   }, [data])
 
   return (
@@ -38,23 +79,23 @@ const LineChartComponent = ({ data }) => {
       <ResponsiveContainer height={475}>
         <LineChart data={data}>
           <Line type="monotone" dataKey="retailSales" stroke="#44A8F6" strokeWidth={4} dot={false} stackId="sales" yAxisId="retail" />
-          <YAxis hide={true} yAxisId="retail" />
+          <YAxis hide={true} domain={[retailYAxis.base, retailYAxis.height]} yAxisId="retail" />
           {showSales.wholesale && (
             <>      
               <Line type="monotone" dataKey="wholesaleSales" stroke="#9AA5BF" strokeWidth={4} dot={false} stackId="sales" yAxisId="wholesale" />}
-              <YAxis hide={true} yAxisId="wholesale" />
+              <YAxis hide={true} domain={[wholesaleYAxis.base, wholesaleYAxis.height]} yAxisId="wholesale" />
             </>
           )}
           {showSales.unitsSold && (
             <>
               <Line type="monotone" dataKey="unitsSold" stroke="#F69244" strokeWidth={4} dot={false} stackId="sales" yAxisId="units" />
-              <YAxis hide={true} yAxisId="units" />  
+              <YAxis hide={true} domain={[unitsSoldYAxis.base, unitsSoldYAxis.height]} yAxisId="units" />  
             </>
           )}
           {showSales.retailerMargin && (
             <>
               <Line type="monotone" dataKey="retailerMargin" stroke="#Eb44F6" strokeWidth={4} dot={false} stackId="sales" yAxisId="margin" />
-              <YAxis hide={true} yAxisId="margin" />
+              <YAxis hide={true} domain={[retailerMarginYAxis.base, retailerMarginYAxis.height]} yAxisId="margin" />
             </>
           )}
           <XAxis hide={true} dataKey="weekEnding" padding={{ left: 35, right: 35 }} />

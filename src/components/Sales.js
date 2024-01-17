120ac16d34a66c9dadc6d009b9101539588881cf
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 10:43:43 -0500

    Update Sales.js

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -7,30 +7,18 @@ const Sales = () => {
   const { sales, ...data } = useSelector(state => state.data.retailSales).at(0)
   const totalMonetaryValue = sales.reduce((acc, val) => acc + val.retailSales + val.wholesaleSales + val.retailerMargin, 0)
   const totalUnitsSold = sales.reduce((acc, val) => acc + val.unitsSold, 0)
-
-  console.log('sales: ', sales)
-  console.log('totalMonetaryValue: ', totalMonetaryValue)
-  console.log('totalUnitsSold: ', totalUnitsSold)
-  
   const percentages = sales.map(item => ({
-    weekEnding: item.weekEnding,
     retailSales: item.retailSales / totalMonetaryValue * 100,
     wholesaleSales: item.wholesaleSales / totalMonetaryValue * 100,
     unitsSold: item.unitsSold / totalUnitsSold * 100,
     retailerMargin: item.retailerMargin / totalMonetaryValue * 100
   }))
-
-  console.log('percentages: ', percentages)
-
-  const maxPercentage = Math.max(...percentages.map(item => {
-    const {weekEnding, ...rest} = item
-    return Object.values(rest)
-  }).flat()) //.filter(x => x !== 0))
+  const maxPercentage = Math.max(...percentages.map(item => Object.values(rest)).flat())
 
   console.log('maxPercentage: ', maxPercentage)
 
-  const normalizedData = percentages.map(item => ({
-    weekEnding: item.weekEnding,
+  const normalizedData = percentages.map((item, index) => ({
+    weekEnding: sales[index].weekEnding,
     retailSales: item.retailSales / maxPercentage,
     wholesaleSales: item.wholesaleSales / maxPercentage,
     unitsSold: item.unitsSold / maxPercentage,

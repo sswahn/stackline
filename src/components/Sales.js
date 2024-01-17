f956e552128c1a4454a73387a04d3dfee0e5906a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-17 09:16:41 -0500

    Update Sales.js

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -5,13 +5,22 @@ import Table from './Table'
 
 const Sales = () => {
   const { sales, ...rest } = useSelector(state => state.data.retailSales).at(0)
+  const sum = sales.reduce((acc, val) => acc + val.retailSales + val.wholesaleSales + val.retailerMargin, 0)
+  const normalizedData = sales.map(item => ({
+    retailSales: item.retailSales / sum * 100,
+    wholesaleSales: item.wholesaleSales / sum * 100,
+    unitsSold: item.unitsSold * 100,
+    retailerMargin: item.retailerMargin / sum * 100
+  }))
+
+  console.log('normalizedData: ', normalizedData)
 
   return (
     <div className="retail-sales">
       <Sidebar data={rest} />
       <div>
-        <LineChart data={sales} /> 
-        <Table data={sales} />
+        <LineChart data={normalizedData} /> 
+        <Table data={normalizedData} />
       </div>
     </div>
   )

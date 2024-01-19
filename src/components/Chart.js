0b3c84267c9e5f1201d80583e95a8f898c90513f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-18 23:32:10 -0500

    Update Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -36,7 +36,7 @@ const Chart = ({ data }) => {
     const padding = {}
     const dynamicLines = Object.keys(showSales).filter(key => showSales[key])
     const lines = ['retailSales', ...dynamicLines]
-    const calculatePadding = index => 60 + (lines.length > 1 ? 60 : 0) * (lines.length - 1 - index)
+    const calculatePadding = index => 50 + (lines.length > 1 ? 60 : 0) * (lines.length - 1 - index)
     lines.forEach((line, index) => {
       if (line === 'unitsSold') {
         padding[line] = calculatePadding(index) + 20
